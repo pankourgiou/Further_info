efee03d9adcc4ff905605489e45f7c9f0115f012
L <60981745+pankourgiou@users.noreply.github.com>
2024-11-19 16:42:20 +0200

    Create Serious.py

---
+++ Serious.py
@@ -0,0 +1,1 @@
+print("In the very near future, 1984 book idea where it says that 1 person will be spied by another person, the analogy will become that 1 person will be pointed by sniffers 1/1 by another person this is WW3 and cannibalism.")
